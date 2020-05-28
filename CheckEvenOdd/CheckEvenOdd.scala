a2b4e5a61231909893458d6957e75f834fe06c31
Thashwini <56554336+Thashwini@users.noreply.github.com>
2020-05-28 22:26:34 +0530

    question 4 edited

---
+++ CheckEvenOdd/CheckEvenOdd.scala
@@ -0,0 +1,29 @@
+
+object CheckEvenOdd extends App{
+  def isEven(x:Int):Boolean=x match{
+    case 0 => true
+    case _ => isOdd(x-1)
+   
+  }
+  def isOdd(x:Int):Boolean= !(isEven(x))
+  
+  println(isEven(8))
+  println(isEven(3))
+  println(isOdd(5))
+  println(isOdd(4))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
